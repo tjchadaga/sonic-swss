350c25b638add3e7d12c2510d97e680dbaa0d6bf
Stepan Blyschak <stepanb@nvidia.com>
2025-11-01 00:07:16 +0200

    move host if test to own test class
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

--- tests/mock_tests/portsorch_ut.cpp
+++ tests/mock_tests/portsorch_ut.cpp
@@ -2925,45 +2925,6 @@ namespace portsorch_test
         }
     }
 
-    TEST_F(PortsOrchTest, PortHostIfCreateFailed)
-    {
-        Table portTable = Table(m_app_db.get(), APP_PORT_TABLE_NAME);
-
-        auto original_api = sai_hostif_api->create_hostif;
-        auto hostIfSpy = SpyOn<SAI_API_HOSTIF, SAI_OBJECT_TYPE_HOSTIF>(&sai_hostif_api->create_hostif);
-        hostIfSpy->callFake([&](sai_object_id_t*, sai_object_id_t, uint32_t, const sai_attribute_t*) -> sai_status_t {
-                return SAI_STATUS_INSUFFICIENT_RESOURCES;
-            }
-        );
-
-        // Get SAI default ports to populate DB
-
-        auto ports = ut_helper::getInitialSaiPorts();
-
-        // Populate pot table with SAI ports
-        for (const auto &it : ports)
-        {
-            portTable.set(it.first, it.second);
-        }
-
-        // Set PortConfigDone
-        portTable.set("PortConfigDone", { { "count", to_string(ports.size()) } });
-
-        gPortsOrch->addExistingData(&portTable);
-
-        // Apply configuration :
-        //  create ports
-
-        static_cast<Orch *>(gPortsOrch)->doTask();
-
-        sai_hostif_api->create_hostif = original_api;
-
-        Port port;
-        gPortsOrch->getPort("Ethernet0", port);
-
-        ASSERT_FALSE(port.m_init);
-    }
-
     TEST_F(PortsOrchTest, PortsWithNoPGsQueuesSchedulerGroups)
     {
         Table portTable = Table(m_app_db.get(), APP_PORT_TABLE_NAME);
@@ -3976,4 +3937,48 @@ namespace portsorch_test
         stateDbSet = stateTable.hget("Ethernet0", "max_priority_groups", value);
         ASSERT_TRUE(stateDbSet);
     }
+
+    struct PortsOrchNegativeTests : PortsOrchTest
+    {
+    };
+
+    TEST_F(PortsOrchNegativeTests, PortHostIfCreateFailed)
+    {
+        Table portTable = Table(m_app_db.get(), APP_PORT_TABLE_NAME);
+
+        auto original_api = sai_hostif_api->create_hostif;
+        auto hostIfSpy = SpyOn<SAI_API_HOSTIF, SAI_OBJECT_TYPE_HOSTIF>(&sai_hostif_api->create_hostif);
+        hostIfSpy->callFake([&](sai_object_id_t*, sai_object_id_t, uint32_t, const sai_attribute_t*) -> sai_status_t {
+                return SAI_STATUS_INSUFFICIENT_RESOURCES;
+            }
+        );
+
+        // Get SAI default ports to populate DB
+
+        auto ports = ut_helper::getInitialSaiPorts();
+
+        // Populate pot table with SAI ports
+        for (const auto &it : ports)
+        {
+            portTable.set(it.first, it.second);
+        }
+
+        // Set PortConfigDone
+        portTable.set("PortConfigDone", { { "count", to_string(ports.size()) } });
+
+        gPortsOrch->addExistingData(&portTable);
+
+        // Apply configuration :
+        //  create ports
+
+        static_cast<Orch *>(gPortsOrch)->doTask();
+
+        sai_hostif_api->create_hostif = original_api;
+
+        Port port;
+        gPortsOrch->getPort("Ethernet0", port);
+
+        ASSERT_FALSE(port.m_init);
+    }
+
 }
